44ab0236034c536514dd4f45553bda70a7ef8e5e
kevinnchristian <kevin.cmribeiro@outlook.com>
2022-09-19 11:11:15 -0300

    :construction: feat: implement routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "../views/Home";
 import Contact from "../views/Contact";
+import SignIn from "../views/SignIn";
+import SignUp from "../views/SignUp";
 
 const RoutesApp: React.FC = () => {
   return (
@@ -10,6 +12,8 @@ const RoutesApp: React.FC = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
