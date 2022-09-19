b9cb38fecfdc464342f295801c5412bbcf3490c3
kevinnchristian <kevin.cmribeiro@outlook.com>
2022-09-19 11:12:13 -0300

    :construction: feat: add index file app

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyle from './global/styles/style';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +11,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
+    <ToastContainer />
     <GlobalStyle />
     <App />
   </React.StrictMode>
