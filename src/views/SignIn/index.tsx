a7159d335b8058a3a2cc73c45ef7de653c27c4d5
kevinnchristian <kevin.cmribeiro@outlook.com>
2022-09-19 11:10:22 -0300

    :construction: feat: implement signin view

---
+++ src/views/SignIn/index.tsx
@@ -0,0 +1,76 @@
+import React, { useState, useCallback, FormEvent } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import Loader from '../../components/Loader';
+import { Container } from './style';
+import { api } from '../../services/api';
+
+interface IData {
+  email: string;
+  password: string;
+}
+
+const SignIn: React.FC = () => {
+  const [data, setData] = useState<IData>({} as IData);
+  const [load, setLoad] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = useCallback((e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoad(true);
+
+    api.post('auth/login', data)
+      .then(response => {
+        const sessionToken = JSON.stringify(response.data.token);
+        localStorage.setItem('@gamaServiceToken', sessionToken);
+        setLoad(false);
+        toast.success('Login realizado com sucesso!', {
+          hideProgressBar: false,
+          onClose: () => navigate('/dashbord')
+        });
+      })
+      .catch(e => {
+        toast.error(e.response.data.msg);
+        setLoad(false);
+      })
+  }, [data, navigate]);
+
+  if (load) {
+    return <Loader />;
+  }
+
+  return (
+    <Container>
+      <div className='card'>
+        <h5>Acesse o sistema:</h5>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            id="user_email"
+            placeholder="Informe seu email"
+            onChange={e => setData({ ...data, email: e.target.value })}
+          />
+          <input
+            type="password"
+            id="user_password"
+            placeholder="Informe sua senha"
+            onChange={e => setData({ ...data, password: e.target.value })}
+          />
+          <input
+            type="submit"
+            value="Entrar"
+          />
+        </form>
+
+        <Link
+          to="/signup"
+          className="anchor-signup"
+        >
+          Clique aqui para cadastrar
+        </Link>
+      </div>
+    </Container>
+  );
+}
+
+export default SignIn;
---
+++ src/views/SignIn/style.ts
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+  align-items: center;
+  background: #8253e5;
+
+  .card {
+    background: #F9F9F9;
+    width: 300px;
+    border-radius: 8px;
+    text-align: center;
+    padding: 24px;
+
+    h5 {
+      color: #8c52e5;
+      font-size: 1.4em;
+      text-align: center;
+      margin-bottom: 10px;
+    }
+
+    form {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-bottom: 15px;
+
+      input {
+        width: 100%;
+        border-radius: 4px;
+        padding: 6px;
+        margin: 5px 0;
+      }
+
+      input[type="text"], input[type="email"], input[type="password"] {
+        border: 1px solid #c2c9d6;
+        
+        &:focus {
+          border: 2px solid #68DE5A;
+          outline: none;
+        }
+      }
+
+      input[type="submit"] {
+        width: 150px;
+        background: #68DE5A;
+        border: none;
+        font-weight: bold;
+        margin-top: 15px;
+        transition: 0.6s;
+
+        &:hover {
+          color: #F9F9F9;
+          background: #8c52e5;
+          cursor: pointer;
+        }
+      }
+    }
+
+    .anchor-signup {
+      color: #8c52e5;
+      font-weight: bold;
+      text-decoration: none;
+      transition: 0.6s;
+
+      &:hover {
+        color: #68DE5A;
+      }
+    }
+  }
+`
